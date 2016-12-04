32a4c537fd81e95a5d5f565242615426d9a8192c
magistr <x2wing@gmail.com>
2016-12-04 11:21:10 +0500

    udate 04.12.2016

--- Cursach/Cursach/Form1.Designer.cs
+++ Cursach/Cursach/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 147);
+            this.label1.Location = new System.Drawing.Point(70, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Входные данные:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -50,24 +51,27 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(94, 241);
+            this.label2.Location = new System.Drawing.Point(62, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.Size = new System.Drawing.Size(103, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Выходные данные:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(270, 147);
+            this.textBox1.Location = new System.Drawing.Point(185, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.Size = new System.Drawing.Size(352, 20);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(541, 144);
+            this.button1.Location = new System.Drawing.Point(598, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(106, 30);
             this.button1.TabIndex = 4;
             this.button1.Text = "Обзор...";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,35 +79,37 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(270, 271);
+            this.listBox1.Location = new System.Drawing.Point(185, 103);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(265, 196);
+            this.listBox1.Size = new System.Drawing.Size(352, 381);
             this.listBox1.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(617, 457);
+            this.button2.Location = new System.Drawing.Point(598, 229);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.Size = new System.Drawing.Size(106, 30);
             this.button2.TabIndex = 6;
             this.button2.Text = "Результат";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(589, 326);
+            this.button3.Location = new System.Drawing.Point(598, 103);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(167, 43);
+            this.button3.Size = new System.Drawing.Size(106, 30);
             this.button3.TabIndex = 7;
             this.button3.Text = "Сохранить файл...";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 516);
+            this.ClientSize = new System.Drawing.Size(752, 500);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
@@ -111,6 +117,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Cursach/Cursach/Processing.cs
+++ Cursach/Cursach/Processing.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cursach
 {
-    class Processing
+    class PROCESSING
     {
     }
 }
