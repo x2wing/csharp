9110f675e172e5ae283941aa6818ed7cfe337ae5
x2wing <x2wing@gmail.com>
2017-05-21 14:47:24 +0500

    add wmi test project

---
+++ wmi21.05/testapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WMISample
+{
+    public class MyWMIQuery
+    {
+        public static void Main()
+        {
+            try
+            {
+                string[] arrComputers = { "192.168.88.239", "UFA-URAL-5", "UFA-URAL-10" };
+                foreach (string strComputer in arrComputers)
+                {
+                    Console.WriteLine("==========================================");
+                    Console.WriteLine("Computer: " + strComputer);
+                    Console.WriteLine("==========================================");
+
+                    ManagementObjectSearcher searcher =
+                        new ManagementObjectSearcher(
+                        "\\\\" + strComputer + "\\root\\CIMV2",
+                        "SELECT * FROM Win32_QuickFixEngineering");
+
+                    foreach (ManagementObject queryObj in searcher.Get())
+                    {
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Win32_QuickFixEngineering instance");
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("HotFixID: {0}", queryObj["HotFixID"]);
+                        Console.WriteLine("InstallDate: {0}", queryObj["InstallDate"]);
+                    }
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException err)
+            {
+                MessageBox.Show("комп выключен или не удалось подключиться" + err.Message);
+            }
+            catch (ManagementException err)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + err.Message);
+            }
+        }
+    }
+}
---
+++ wmi21.05/testapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("testapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testapp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2809931f-af4d-4bf7-8662-e9665fb949bb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
